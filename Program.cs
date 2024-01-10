56b41b5355542e7995c48747553037f77fa1bfde
onkar <jadhavonkar333@gmail.com>
2024-01-10 12:19:02 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -10,4 +10,10 @@ for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine($"{n}*{i}=\t{n * i}");
 }
+Console.WriteLine("table of 7");
+n = 7;
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{n}*{i}=\t{n * i}");
+}
 
